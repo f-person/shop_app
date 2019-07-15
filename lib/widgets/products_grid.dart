d1b29f8fac7ce60780b9068bb645231c959f993c
f-person <agakaryan.arshak@gmail.com>
2019-07-15 23:22:01 +0400

    Extract ProductsGrid. Use Provider to get data for ProductsGrid.

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final loadedProducts = Provider.of<Products>(context).items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: loadedProducts.length,
+      itemBuilder: (ctx, i) {
+        return ProductItem(
+          loadedProducts[i].id,
+          loadedProducts[i].title,
+          loadedProducts[i].imageUrl,
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10.0,
+      ),
+    );
+  }
+}
