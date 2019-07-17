f3af06c12251e477a46c9dca85741c495e92afe9
f-person <agakaryan.arshak@gmail.com>
2019-07-17 09:00:48 +0400

    Implement body of ProductDetailScreen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -16,6 +16,32 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Image.network(selectedProduct.imageUrl, fit: BoxFit.cover),
+              height: 300.0,
+              width: double.infinity,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '\$${selectedProduct.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20.0),
+            ),
+            SizedBox(height: 10.0),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                selectedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
